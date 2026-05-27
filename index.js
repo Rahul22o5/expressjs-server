f0c5c9e9b8004629a3e140e1893d82f76350c0d1
Rahul Mula <rahulmula.lm10@gmail.com>
2026-05-27 21:55:15 +0530

    Question_Routes

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 let port=3000;
 
-function addUser(req,res,next){
-    req.user = "Guest";
-    next();
-}
-app.get('/welcome',addUser,(req,res) =>{
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
-    
+//get orders
+app.get('/orders',(req,res) =>{
+    res.send("Here is the list of all orders.");
 })
+//post orders
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+//get users
+app.get('/users',(req,res) =>{
+    res.send("Here is the list of all users");
+})
+//post users
+app.post("/users", (req, res) => {
+  res.send("A new user has been created.");
+});
+
 
 app.listen(port,()=>{
     console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
