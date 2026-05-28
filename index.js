f97f0e24d829f54fe6b5e9c71e310a723986e349
Rahul Mula <rahulmula.lm10@gmail.com>
2026-05-28 13:35:17 +0530

    dynamic routes

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const express = require("express");
 const app = express();
-let port=4000;
+let port=3000;
 
-//get products
-app.get('/products',(req,res) =>{
-    res.send("Here is the list of all products.");
+//dynamic get
+app.get('/welcome/:username',(req,res) =>{
+  const username = req.params.username;
+  const role=req.query.role
+  res.send(`Welcome, ${username}! Your role is ${role}.`);
 })
 
-//post products
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-
-//get categories
-app.get('/categories',(req,res) =>{
-    res.send("Here is the list of all categories");
-})
-//post categories
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
-
-//wilcard route
-app.use((req,res)=>{
-  res.status(404).send("<h1>404 - Page Not Found</h1>")
-})
 
 app.listen(port,()=>{
     console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
