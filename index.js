ad521c58d327638749d1429ab5e32c78b0a6f33d
Rahul Mula <rahulmula.lm10@gmail.com>
2026-05-28 12:43:26 +0530

    question with unknown routes

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-let port=3000;
+let port=4000;
 
-//get orders
-app.get('/orders',(req,res) =>{
-    res.send("Here is the list of all orders.");
+//get products
+app.get('/products',(req,res) =>{
+    res.send("Here is the list of all products.");
 })
-//post orders
-app.post("/orders", (req, res) => {
-  res.send("A new order has been created.");
+
+//post products
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
 });
 
-//get users
-app.get('/users',(req,res) =>{
-    res.send("Here is the list of all users");
+//get categories
+app.get('/categories',(req,res) =>{
+    res.send("Here is the list of all categories");
 })
-//post users
-app.post("/users", (req, res) => {
-  res.send("A new user has been created.");
+//post categories
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
 });
 
+//wilcard route
+app.use((req,res)=>{
+  res.status(404).send("<h1>404 - Page Not Found</h1>")
+})
 
 app.listen(port,()=>{
     console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
